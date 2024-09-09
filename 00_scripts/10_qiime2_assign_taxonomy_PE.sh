9e9f5ab46aebed486deff89d5a92144d505b63d0
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2024-09-09 16:57:38 +0200

    Update 10_qiime2_assign_taxonomy_PE.sh

--- 00_scripts/10_qiime2_assign_taxonomy_PE.sh
+++ 00_scripts/10_qiime2_assign_taxonomy_PE.sh
@@ -29,149 +29,149 @@ TMPDIR=/home
 #### Aim: Import data to create a new QIIME 2 Artifact
 #### https://gitlab.com/IAC_SolVeg/CNRT_BIOINDIC/-/blob/master/snk/12_qiime2_taxonomy
 ###
-###############################################################
-### For Fungi
-###############################################################
-
-cd $DATADIRECTORY_ITS
-
-eval "$(conda shell.bash hook)"
-conda activate qiime2-2021.4
-
-# Make the directory (mkdir) only if not existe already(-p)
-mkdir -p taxonomy
-mkdir -p export/taxonomy
-
-# I'm doing this step in order to deal the no space left in cluster :
-export TMPDIR='/home/fungi'
-echo $TMPDIR
-
-#print 'ls /home/fungi'
-#ls /home/fungi
-
-#env
-
-#df -h
-
-
-# NEW DATABASE UNITE :
-# sh_taxonomy_qiime_ver8_dynamic_s_10.05.2021.txt
-# sh_refs_qiime_ver8_dynamic_s_10.05.2021.fasta
-# from https://plutof.ut.ee/#/doi/10.15156/BIO/1264763
-# Originaly from https://unite.ut.ee/repository.php
-# When using this resource, please cite it as follows:
-# Abarenkov, Kessy; Zirk, Allan; Piirmann, Timo; Pöhönen, Raivo; Ivanov, Filipp; Nilsson, R. Henrik; Kõljalg, Urmas (2021): UNITE QIIME release for Fungi 2. Version 10.05.2021. UNITE Community. https://doi.org/10.15156/BIO/1264763 
-# Includes global and 97% singletons.
-
-# OLD = /home/fungi/Diversity_in_Mare_yam_crop/98_database_files/ITS2/Taxonomy-UNITE-V7-S-2017.12.01-dynamic.txt
-
-qiime tools import --type 'FeatureData[Taxonomy]' \
-  --input-format HeaderlessTSVTaxonomyFormat \
-  --input-path /home/fungi/Mayotte_microorganism_colonisation/98_database_files/sh_taxonomy_qiime_ver8_dynamic_s_10.05.2021.txt \
-  --output-path taxonomy/RefTaxo.qza
-
-# You will need to importe the "Sequence-UNITE-V7-S-2017.12.01-dynamic.fasta" file by yourself because it's to big for beeing upload by GitHub.
-# You can donwload it from here : https://gitlab.com/IAC_SolVeg/CNRT_BIOINDIC/-/tree/master/inp/qiime2/taxonomy/ITS
-
-# OLD = /home/fungi/Diversity_in_Mare_yam_crop/98_database_files/ITS2/Sequence-UNITE-V7-S-2017.12.01-dynamic.fasta
-
-qiime tools import --type 'FeatureData[Sequence]' \
-  --input-path /home/fungi/Mayotte_microorganism_colonisation/98_database_files/sh_refs_qiime_ver8_dynamic_s_10.05.2021.fasta \
-  --output-path taxonomy/DataSeq.qza
-
-# Fungal ITS classifiers trained on the UNITE reference database do NOT benefit
-# from extracting / trimming reads to primer sites.
-# We recommend training UNITE classifiers on the full reference sequences !!!
-
-# Furthermore, we recommend the 'developer' sequences
-# (located within the QIIME-compatible release download),
-# because the standard versions of the sequences have already been trimmed to
-# the ITS region, excluding portions of flanking rRNA genes that may be present
-# in amplicons generated with standard ITS primers.
-
-# Aim: Rename import ITS DataSeq in ITS RefSeq for training.
-
-cp taxonomy/DataSeq.qza taxonomy/RefSeq.qza
-
-# Now in order to deal with the "no left space" problem, we will sned temporarly the files in the SCRATCH part of the cluster, I directly did this step in local and then upload the file in cluster
-
-# Aim: Create a scikit-learn naive_bayes classifier for reads
-
-qiime feature-classifier fit-classifier-naive-bayes \
-  --i-reference-reads taxonomy/RefSeq.qza \
-  --i-reference-taxonomy taxonomy/RefTaxo.qza \
-  --o-classifier taxonomy/Classifier.qza
-
-# Aim: Classify reads by taxon using a fitted classifier
-# --p-reads-per-batch 1000
-
-qiime feature-classifier classify-sklearn \
-   --i-classifier taxonomy/Classifier.qza \
-   --i-reads core/ConRepSeq.qza \
-   --o-classification taxonomy/taxonomy_reads-per-batch_ConRepSeq.qza
-   
-qiime feature-classifier classify-sklearn \
-  --i-classifier taxonomy/Classifier.qza \
-  --i-reads core/RepSeq.qza \
-  --o-classification taxonomy/taxonomy_reads-per-batch_RepSeq.qza
-
-qiime feature-classifier classify-sklearn \
-  --i-classifier taxonomy/Classifier.qza \
-  --i-reads core/RarRepSeq.qza \
-  --o-classification taxonomy/taxonomy_reads-per-batch_RarRepSeq.qza
-
-# Switch to https://chmi-sops.github.io/mydoc_qiime2.html#step-9-assign-taxonomy
-# --p-reads-per-batch 0 (default)
-
-qiime metadata tabulate \
-  --m-input-file taxonomy/taxonomy_reads-per-batch_RarRepSeq.qza \
-  --o-visualization taxonomy/taxonomy_reads-per-batch_RarRepSeq.qzv
-
-qiime metadata tabulate \
-  --m-input-file taxonomy/taxonomy_reads-per-batch_ConRepSeq.qza \
-  --o-visualization taxonomy/taxonomy_reads-per-batch_ConRepSeq.qzv
-  
-qiime metadata tabulate \
-  --m-input-file taxonomy/taxonomy_reads-per-batch_RepSeq.qza \
-  --o-visualization taxonomy/taxonomy_reads-per-batch_RepSeq.qzv  
-
-
-# Now create a visualization of the classified sequences.
-  
-qiime taxa barplot \
-  --i-table core/Table.qza \
-  --i-taxonomy taxonomy/taxonomy_reads-per-batch_RepSeq.qza \
-  --m-metadata-file $METADATA_ITS/sample-metadata.tsv \
-  --o-visualization taxonomy/taxa-bar-plots_reads-per-batch_RepSeq.qzv
-
-qiime taxa barplot \
-  --i-table core/ConTable.qza \
-  --i-taxonomy taxonomy/taxonomy_reads-per-batch_ConRepSeq.qza \
-  --m-metadata-file $METADATA_ITS/sample-metadata.tsv \
-  --o-visualization taxonomy/taxa-bar-plots_reads-per-batch_ConRepSeq.qzv
-  
-qiime taxa barplot \
-  --i-table core/RarTable.qza \
-  --i-taxonomy taxonomy/taxonomy_reads-per-batch_RarRepSeq.qza \
-  --m-metadata-file $METADATA_ITS/sample-metadata.tsv \
-  --o-visualization taxonomy/taxa-bar-plots_reads-per-batch_RarRepSeq.qzv  
-
-qiime tools export --input-path taxonomy/Classifier.qza --output-path export/taxonomy/Classifier
-qiime tools export --input-path taxonomy/RefSeq.qza --output-path export/taxonomy/RefSeq
-qiime tools export --input-path taxonomy/DataSeq.qza --output-path export/taxonomy/DataSeq
-qiime tools export --input-path taxonomy/RefTaxo.qza --output-path export/taxonomy/RefTaxo
-  
-qiime tools export --input-path taxonomy/taxa-bar-plots_reads-per-batch_RarRepSeq.qzv --output-path export/taxonomy/taxa-bar-plots_reads-per-batch_RarRepSeq
-qiime tools export --input-path taxonomy/taxa-bar-plots_reads-per-batch_ConRepSeq.qzv --output-path export/taxonomy/taxa-bar-plots_reads-per-batch_ConRepSeq
-qiime tools export --input-path taxonomy/taxa-bar-plots_reads-per-batch_RepSeq.qzv --output-path export/taxonomy/taxa-bar-plots_reads-per-batch_RepSeq
-
-qiime tools export --input-path taxonomy/taxonomy_reads-per-batch_RepSeq.qzv --output-path export/taxonomy/taxonomy_reads-per-batch_RepSeq_visual
-qiime tools export --input-path taxonomy/taxonomy_reads-per-batch_ConRepSeq.qzv --output-path export/taxonomy/taxonomy_reads-per-batch_ConRepSeq_visual
-qiime tools export --input-path taxonomy/taxonomy_reads-per-batch_RarRepSeq.qzv --output-path export/taxonomy/taxonomy_reads-per-batch_RarRepSeq_visual
-
-qiime tools export --input-path taxonomy/taxonomy_reads-per-batch_RepSeq.qza --output-path export/taxonomy/taxonomy_reads-per-batch_RepSeq
-qiime tools export --input-path taxonomy/taxonomy_reads-per-batch_ConRepSeq.qza --output-path export/taxonomy/taxonomy_reads-per-batch_ConRepSeq
-qiime tools export --input-path taxonomy/taxonomy_reads-per-batch_RarRepSeq.qza --output-path export/taxonomy/taxonomy_reads-per-batch_RarRepSeq
+### ###############################################################
+### ### For Fungi
+### ###############################################################
+### 
+### cd $DATADIRECTORY_ITS
+### 
+### eval "$(conda shell.bash hook)"
+### conda activate qiime2-2021.4
+### 
+### # Make the directory (mkdir) only if not existe already(-p)
+### mkdir -p taxonomy
+### mkdir -p export/taxonomy
+### 
+### # I'm doing this step in order to deal the no space left in cluster :
+### export TMPDIR='/home/fungi'
+### echo $TMPDIR
+### 
+### #print 'ls /home/fungi'
+### #ls /home/fungi
+### 
+### #env
+### 
+### #df -h
+### 
+### 
+### # NEW DATABASE UNITE :
+### # sh_taxonomy_qiime_ver8_dynamic_s_10.05.2021.txt
+### # sh_refs_qiime_ver8_dynamic_s_10.05.2021.fasta
+### # from https://plutof.ut.ee/#/doi/10.15156/BIO/1264763
+### # Originaly from https://unite.ut.ee/repository.php
+### # When using this resource, please cite it as follows:
+### # Abarenkov, Kessy; Zirk, Allan; Piirmann, Timo; Pöhönen, Raivo; Ivanov, Filipp; Nilsson, R. Henrik; Kõljalg, Urmas (2021): UNITE QIIME release for Fungi 2. Version 10.05.2021. UNITE Community. https://doi.org/10.15156/BIO/1264763 
+### # Includes global and 97% singletons.
+### 
+### # OLD = /home/fungi/Diversity_in_Mare_yam_crop/98_database_files/ITS2/Taxonomy-UNITE-V7-S-2017.12.01-dynamic.txt
+### 
+### qiime tools import --type 'FeatureData[Taxonomy]' \
+###   --input-format HeaderlessTSVTaxonomyFormat \
+###   --input-path /home/fungi/Mayotte_microorganism_colonisation/98_database_files/sh_taxonomy_qiime_ver8_dynamic_s_10.05.2021.txt \
+###   --output-path taxonomy/RefTaxo.qza
+### 
+### # You will need to importe the "Sequence-UNITE-V7-S-2017.12.01-dynamic.fasta" file by yourself because it's to big for beeing upload by GitHub.
+### # You can donwload it from here : https://gitlab.com/IAC_SolVeg/CNRT_BIOINDIC/-/tree/master/inp/qiime2/taxonomy/ITS
+### 
+### # OLD = /home/fungi/Diversity_in_Mare_yam_crop/98_database_files/ITS2/Sequence-UNITE-V7-S-2017.12.01-dynamic.fasta
+### 
+### qiime tools import --type 'FeatureData[Sequence]' \
+###   --input-path /home/fungi/Mayotte_microorganism_colonisation/98_database_files/sh_refs_qiime_ver8_dynamic_s_10.05.2021.fasta \
+###   --output-path taxonomy/DataSeq.qza
+### 
+### # Fungal ITS classifiers trained on the UNITE reference database do NOT benefit
+### # from extracting / trimming reads to primer sites.
+### # We recommend training UNITE classifiers on the full reference sequences !!!
+### 
+### # Furthermore, we recommend the 'developer' sequences
+### # (located within the QIIME-compatible release download),
+### # because the standard versions of the sequences have already been trimmed to
+### # the ITS region, excluding portions of flanking rRNA genes that may be present
+### # in amplicons generated with standard ITS primers.
+### 
+### # Aim: Rename import ITS DataSeq in ITS RefSeq for training.
+### 
+### cp taxonomy/DataSeq.qza taxonomy/RefSeq.qza
+### 
+### # Now in order to deal with the "no left space" problem, we will sned temporarly the files in the SCRATCH part of the cluster, I directly did this step in local and then upload the file in cluster
+### 
+### # Aim: Create a scikit-learn naive_bayes classifier for reads
+### 
+### qiime feature-classifier fit-classifier-naive-bayes \
+###   --i-reference-reads taxonomy/RefSeq.qza \
+###   --i-reference-taxonomy taxonomy/RefTaxo.qza \
+###   --o-classifier taxonomy/Classifier.qza
+### 
+### # Aim: Classify reads by taxon using a fitted classifier
+### # --p-reads-per-batch 1000
+### 
+### qiime feature-classifier classify-sklearn \
+###    --i-classifier taxonomy/Classifier.qza \
+###    --i-reads core/ConRepSeq.qza \
+###    --o-classification taxonomy/taxonomy_reads-per-batch_ConRepSeq.qza
+###    
+### qiime feature-classifier classify-sklearn \
+###   --i-classifier taxonomy/Classifier.qza \
+###   --i-reads core/RepSeq.qza \
+###   --o-classification taxonomy/taxonomy_reads-per-batch_RepSeq.qza
+### 
+### qiime feature-classifier classify-sklearn \
+###   --i-classifier taxonomy/Classifier.qza \
+###   --i-reads core/RarRepSeq.qza \
+###   --o-classification taxonomy/taxonomy_reads-per-batch_RarRepSeq.qza
+### 
+### # Switch to https://chmi-sops.github.io/mydoc_qiime2.html#step-9-assign-taxonomy
+### # --p-reads-per-batch 0 (default)
+### 
+### qiime metadata tabulate \
+###   --m-input-file taxonomy/taxonomy_reads-per-batch_RarRepSeq.qza \
+###   --o-visualization taxonomy/taxonomy_reads-per-batch_RarRepSeq.qzv
+### 
+### qiime metadata tabulate \
+###   --m-input-file taxonomy/taxonomy_reads-per-batch_ConRepSeq.qza \
+###   --o-visualization taxonomy/taxonomy_reads-per-batch_ConRepSeq.qzv
+###   
+### qiime metadata tabulate \
+###   --m-input-file taxonomy/taxonomy_reads-per-batch_RepSeq.qza \
+###   --o-visualization taxonomy/taxonomy_reads-per-batch_RepSeq.qzv  
+### 
+### 
+### # Now create a visualization of the classified sequences.
+###   
+### qiime taxa barplot \
+###   --i-table core/Table.qza \
+###   --i-taxonomy taxonomy/taxonomy_reads-per-batch_RepSeq.qza \
+###   --m-metadata-file $METADATA_ITS/sample-metadata.tsv \
+###   --o-visualization taxonomy/taxa-bar-plots_reads-per-batch_RepSeq.qzv
+### 
+### qiime taxa barplot \
+###   --i-table core/ConTable.qza \
+###   --i-taxonomy taxonomy/taxonomy_reads-per-batch_ConRepSeq.qza \
+###   --m-metadata-file $METADATA_ITS/sample-metadata.tsv \
+###   --o-visualization taxonomy/taxa-bar-plots_reads-per-batch_ConRepSeq.qzv
+###   
+### qiime taxa barplot \
+###   --i-table core/RarTable.qza \
+###   --i-taxonomy taxonomy/taxonomy_reads-per-batch_RarRepSeq.qza \
+###   --m-metadata-file $METADATA_ITS/sample-metadata.tsv \
+###   --o-visualization taxonomy/taxa-bar-plots_reads-per-batch_RarRepSeq.qzv  
+### 
+### qiime tools export --input-path taxonomy/Classifier.qza --output-path export/taxonomy/Classifier
+### qiime tools export --input-path taxonomy/RefSeq.qza --output-path export/taxonomy/RefSeq
+### qiime tools export --input-path taxonomy/DataSeq.qza --output-path export/taxonomy/DataSeq
+### qiime tools export --input-path taxonomy/RefTaxo.qza --output-path export/taxonomy/RefTaxo
+###   
+### qiime tools export --input-path taxonomy/taxa-bar-plots_reads-per-batch_RarRepSeq.qzv --output-path export/taxonomy/taxa-bar-plots_reads-per-batch_RarRepSeq
+### qiime tools export --input-path taxonomy/taxa-bar-plots_reads-per-batch_ConRepSeq.qzv --output-path export/taxonomy/taxa-bar-plots_reads-per-batch_ConRepSeq
+### qiime tools export --input-path taxonomy/taxa-bar-plots_reads-per-batch_RepSeq.qzv --output-path export/taxonomy/taxa-bar-plots_reads-per-batch_RepSeq
+### 
+### qiime tools export --input-path taxonomy/taxonomy_reads-per-batch_RepSeq.qzv --output-path export/taxonomy/taxonomy_reads-per-batch_RepSeq_visual
+### qiime tools export --input-path taxonomy/taxonomy_reads-per-batch_ConRepSeq.qzv --output-path export/taxonomy/taxonomy_reads-per-batch_ConRepSeq_visual
+### qiime tools export --input-path taxonomy/taxonomy_reads-per-batch_RarRepSeq.qzv --output-path export/taxonomy/taxonomy_reads-per-batch_RarRepSeq_visual
+### 
+### qiime tools export --input-path taxonomy/taxonomy_reads-per-batch_RepSeq.qza --output-path export/taxonomy/taxonomy_reads-per-batch_RepSeq
+### qiime tools export --input-path taxonomy/taxonomy_reads-per-batch_ConRepSeq.qza --output-path export/taxonomy/taxonomy_reads-per-batch_ConRepSeq
+### qiime tools export --input-path taxonomy/taxonomy_reads-per-batch_RarRepSeq.qza --output-path export/taxonomy/taxonomy_reads-per-batch_RarRepSeq
 
   
 
@@ -262,8 +262,8 @@ echo '##########################################################################
 ###cp $METADATA_16S/SILVA-138-SSURef-Full-Seqs.qza taxonomy/DataSeq.qza
 ###cp $METADATA_16S/Silva-v138-full-length-seq-taxonomy.qza taxonomy/RefTaxo.qza
 
-cp /home/fungi/Mayotte_microorganism_colonisation/98_database_files/SILVA-138-SSURef-Full-Seqs.qza taxonomy/16S/DataSeq.qza
-cp /home/fungi/Mayotte_microorganism_colonisation/98_database_files/Silva-v138-full-length-seq-taxonomy.qza taxonomy/16S/RefTaxo.qza
+cp /home/fungi/Mayotte_microorganism_colonisation/98_database_files/SILVA-138-SSURef-Full-Seqs.qza taxonomy/DataSeq.qza
+cp /home/fungi/Mayotte_microorganism_colonisation/98_database_files/Silva-v138-full-length-seq-taxonomy.qza taxonomy/RefTaxo.qza
 
 # Here only for 16S --> forward: 'GTGCCAGCMGCCGCGGTAA'  # 515f & reverse: 'GGACTACHVGGGTWTCTAAT' # 806r
 #qiime feature-classifier extract-reads --i-sequences taxonomy/DataSeq.qza \
